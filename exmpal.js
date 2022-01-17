ea61e255aed7f6dcf333981516ca5544a38c3783
chiragthummar <chiragthummar4747@gmail.com>
2022-01-17 18:05:17 +0530

    17/1/22

---
+++ exmpal.js
@@ -0,0 +1,26 @@
+
+//hoisting
+// let
+
+// const x;
+// x = 10
+
+// var x = 10
+// var x = 20
+// console.log("1 x : ", x)
+// console.log("2 x : ", x)
+// var x = 10
+//
+//     var x = 20
+//     console.log("1 x : ", x)
+// }
+// console.log("2 x :", x)
+
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits)
+
+// console.log(fruits.push("kiwi"))
+console.log(fruits.length)
+// console.log(fruits.filter)
