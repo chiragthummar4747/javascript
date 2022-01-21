153e28d97ec66ce4688c7098ea743a900e3ce1b8
chiragthummar <chiragthummar4747@gmail.com>
2022-01-21 10:42:19 +0530

    21/01/22

--- callfun.js
+++ callfun.js
@@ -28,6 +28,7 @@ function total(subject1, subject2, subject3)
   let Totalmarks = subject1 + subject2 + subject3;
   let mark = `hello ${this.name} , total ${Totalmarks}`;
   console.log(mark)
+  // console.log(typeof(total))
 }
 total.call(student1,50,60,70)
 total.call(student2,70,40,50)
